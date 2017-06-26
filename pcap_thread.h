57464741a501f1638947f12d9a22b9f8ef3cee9f
Jerry Lundström <lundstrom.jerry@gmail.com>
2017-06-26 08:39:13 +0200

    Rearrange headers for *BSD

--- pcap_thread.h
+++ pcap_thread.h
@@ -46,9 +46,10 @@
 #include <sys/time.h>
 #include <sys/types.h>
 
+#include <netinet/in.h>
+
 #include <net/if_arp.h>
 #include <netinet/if_ether.h>
-#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/ip6.h>
 
